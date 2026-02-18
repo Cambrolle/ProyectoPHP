a3e5741b49ca0ace25b9873b05e189390e85f8dd
Cambrolle <acambrollemoraza@safareyes.es>
2026-02-18 17:19:18 +0100

    migrations

---
+++ migrations/Version20260210184355.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260210184355 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE UNIQUE INDEX unique_user_category_ranking ON competiciones.ranking (usuario_id, category_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX competiciones.unique_user_category_ranking');
+    }
+}
